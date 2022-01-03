f73608eca2d36b7d7c0e8140bc862bf1c98ad693
Shehab Gad <shehabgad2@gmail.com>
2022-01-03 07:49:12 +0200

    Update database_info.php

--- config/database_info.php
+++ config/database_info.php
@@ -1,5 +1,5 @@
 <?php
-$server = "localhost";
-$username = "shehab";
-$password = "test1234";
-$dbname = "seekers";
+$server = "sql111.epizy.com";
+$username = "epiz_30722142";
+$password = "4DDex0NqhI7u";
+$dbname = "epiz_30722142_seekers";
